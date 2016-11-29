1f31d4027ce03de97677dc84842792ff75daf20c
Farhan Shafi <fshafi@purdue.edu>
2016-11-29 16:08:37 -0500

    added method

--- p4/src/extra.java
+++ p4/src/extra.java
@@ -2,4 +2,20 @@
  * Created by farhanshafi on 11/29/16.
  */
 public class extra {
+
+    public static boolean formatCheck(String request){
+        if (request== "CREATENEWUSER" || request== "LOGIN" || request== "STARTNEWGAME" || request== "JOINGAME" || request== "ALLPARTICIPANTSHAVEJOINED"
+            || request== "PLAYERSUGGESTION" || request== "PLAYERCHOICE"){
+            return true;
+        }
+        else return false;
+    }
+
+    /*public static void main(String[] args) {
+        boolean x;
+        x = formatCheck("test");
+        System.out.println(x);
+    }*/
+
+
 }
